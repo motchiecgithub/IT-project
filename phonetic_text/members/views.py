7dba2650033db7c10df8d364fafcc7107d469070
haochengw1 <127472500+haochengw1@users.noreply.github.com>
2023-08-26 18:25:20 +1000

    Update translate function in views.py

--- phonetic_text/members/views.py
+++ phonetic_text/members/views.py
@@ -23,11 +23,22 @@ def main(request):
   template = loader.get_template('main.html')
   return HttpResponse(template.render())
 
-def testing(request):
-  template = loader.get_template('template.html')
-  # show the returned value from phonetic_soundex and phonetic_metaphone
-  context = {
-    'soundex': phonetic_soundex('phonetic'),
-    'metaphone': phonetic_metaphone('phonetic'),
-  }
-  return HttpResponse(template.render(context, request))
+def translate(request):
+  template = loader.get_template('translate.html')
+  # show the returned value from phonetic_soundex and phonetic_metaphone 
+  if request.method == 'POST':
+    form = TranslationForm(request.POST)
+    if form.is_valid():
+            english_text = form.cleaned_data['english_text']
+            text = phonetic_translate(english_text, "en_US")
+            soundex = phonetic_soundex(english_text)
+            metaphone = phonetic_metaphone(english_text)
+            context = {
+              'text': text,
+              'soundex': soundex,
+              'metaphone': metaphone,
+            }
+            return render(request, 'translate.html', {'form': form, 'context': context})
+  else:
+    form = TranslationForm()
+  return render(request, 'translate.html', {'form': form})
