5a390cc24b29f421db342665c7805a6cbe5a205d
Huy Le <80511724+motchiecgithub@users.noreply.github.com>
2023-10-20 14:35:33 +1100

    implement gpt, refine frontend

--- dictionary/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms 
-
-class WordInputForm(forms.Form):
-    word = forms.CharField(max_length=100)
--- pronunciation/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms 
-
-class WordInputForm(forms.Form):
-    word = forms.CharField(max_length=100, required=False)
-
-class GenerateTextForm(forms.Form):
-    input = forms.CharField(max_length=1000, required=False, 
-                            widget=forms.TextInput(attrs={'autocomplete': 'off'}))
